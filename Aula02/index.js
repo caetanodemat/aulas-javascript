40141e99f3da19f47e84f5b83d08f5e7110a799e
Joao Pedro <joaopedrocaetanodematos@outlook.com>
2024-03-25 21:14:56 -0300

    Aula de variaveis, console, alert, prompt

---
+++ Aula02/index.js
@@ -0,0 +1,58 @@
+
+//var nome 
+//var sobrenome 
+//var idade 
+
+//atribuicão
+//nome = "Pedro"
+//sobrenome = 'Silva'
+//idade = 25
+
+var marca = "Adidas"
+console.log("Marca", marca)
+
+
+var primeiroNumero = 1
+var segundoNumero = 2
+ 
+//Soma de dois valores
+var soma = primeiroNumero + segundoNumero
+console.log("Soma", soma)
+//Subtracao de dois valore
+var subtracao = primeiroNumero - segundoNumero
+console.log("Subtracao", subtracao)
+//Multiplicao de dois valores
+var multiplicao =  primeiroNumero * segundoNumero
+console.log("Multiplicacao", multiplicao)
+//Divisao de dois valores
+var divisao = primeiroNumero / segundoNumero
+console.log("Divisao", divisao)
+//Resto da divisao 
+var resto = primeiroNumero % segundoNumero
+console.log("Resto", resto)
+//Potencia
+var potencia = primeiroNumero ** segundoNumero
+console.log("Potencia", potencia)
+
+var textoA = "Coder"
+var textoB = "House"
+var textoC = " "
+var resultadoA = textoA + " " + textoB
+console.log(resultadoA)
+var resultadoB = textoA + textoC + textoB
+console.log(resultadoB)
+
+var nome = "João"
+var sobreNome = "Matos"
+var anoAtual = 2024
+var idade = 22
+var espaco = " "
+var nomeCompleto = nome + espaco + sobreNome
+console.log("Seu nome completo é:", nomeCompleto)
+var anoNascimento = anoAtual - idade
+console.log("Seu ano de nascimento é:", anoNascimento)
+
+var nomeInserido = prompt("Insira seu nome:")
+console.log("Nome inserido:", nomeInserido)
+
+alert("Seja bem-vindo ao curso de JavaScript")
---
+++ Aula02/index1.js
@@ -0,0 +1,14 @@
+
+//var entrada = prompt("Insira uma letra")
+//var saida = entrada + " " + "info inserida"
+//alert(saida)
+
+//var valorString = prompt("Insira um numero:")
+//var valorConvertido = parseInt(valorString)
+//console.log(valorConvertido)
+
+var numeroUm = parseFloat(prompt("Insira o primeiro numero:"))
+var numeroDois = parseFloat(prompt("Insira o segundo numero:"))
+console.log(numeroUm + numeroDois)
+
+alert(numeroUm * numeroDois)
