d331d681758290deb1ba139db5c778c8ba299c31
Joao Pedro <joaopedrocaetanodematos@outlook.com>
2024-04-09 20:33:03 -0300

    workshop

---
+++ Workshop1/index.js
@@ -0,0 +1,12 @@
+//var soma = function () { }
+//console.log(soma(10,30))
+
+var resultado = 0
+
+function somar(primeiroNumero, segundoNumero){
+
+  resultado = primeiroNumero + segundoNumero
+}
+
+somar(10,20)
+console.log("Resultado", resultado)
